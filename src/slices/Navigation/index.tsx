549dd7f245caffae03dd3b85e14c80dc5f22d10c
lunalubra <lucas2002luna@gmail.com>
2024-11-28 23:09:57 +0100

    navigation updates

--- src/slices/Navigation/index.tsx
+++ src/slices/Navigation/index.tsx
@@ -56,7 +56,7 @@ const DesktopNavigation = ({ slice }: { slice: NavigationProps["slice"] }) => {
           />
         ))}
       </div>
-      <div className="w-full flex items-center justify-around">
+      <div className="flex items-center min-w-[200px] justify-center">
         <DesktopNavigationItem
           link={middleSection.link}
           image={middleSection.image}
