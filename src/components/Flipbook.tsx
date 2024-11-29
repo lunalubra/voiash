d95a1c9e72c90967527073e8ff926a3177f2cb20
lunalubra <lucas2002luna@gmail.com>
2024-11-29 12:36:22 +0100

    add arrows to the flipbook

--- src/components/Flipbook.tsx
+++ src/components/Flipbook.tsx
@@ -1,25 +1,64 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import HTMLFlipBook from "react-pageflip";
 import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import dynamic from "next/dynamic";
+import Arrow from "../images/Arrow";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
 
 function Flipbook({ pdfLink }: { pdfLink: string }) {
   const [numPages, setNumPages] = useState(5);
+  const flipbookRef = useRef<any>(null);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
     setNumPages(numPages);
   }
 
+  function handleGoNext() {
+    console.log(flipbookRef.current.pageFlip());
+    flipbookRef.current.pageFlip().flipNext();
+  }
+
+  function handleGoPrev() {
+    flipbookRef.current.pageFlip().flipPrev();
+  }
+
   return (
     <div className="w-full flex items-center justify-center">
-      <div className="hidden md:flex w-full items-center justify-center">
+      <div className="hidden md:flex w-full items-center justify-center gap-4">
+        <div onClick={handleGoPrev}>
+          <Arrow />
+        </div>
         {/* @ts-ignore */}
-        <HTMLFlipBook width={400} height={570} showPageCorners={false}>
+        <HTMLFlipBook
+          ref={flipbookRef}
+          width={400}
+          height={570}
+          showPageCorners={false}
+          // className={""}
+          // style={undefined}
+          // startPage={0}
+          // size={"fixed"}
+          // minWidth={0}
+          // maxWidth={0}
+          // minHeight={0}
+          // maxHeight={0}
+          // drawShadow={false}
+          // flippingTime={0}
+          // usePortrait={false}
+          // startZIndex={0}
+          // autoSize={false}
+          // maxShadowOpacity={0}
+          // showCover={false}
+          // mobileScrollSupport={false}
+          // clickEventForward={false}
+          // useMouseEvents={false}
+          // swipeDistance={0}
+          // disableFlipByClick={false}
+        >
           {[...Array(numPages).keys()].map((pNum) => (
             <div key={pNum}>
               <Document file={pdfLink} onLoadSuccess={onDocumentLoadSuccess}>
@@ -36,6 +75,9 @@ function Flipbook({ pdfLink }: { pdfLink: string }) {
             </div>
           ))}
         </HTMLFlipBook>
+        <div onClick={handleGoNext} className="rotate-180">
+          <Arrow />
+        </div>
       </div>
       <div className="flex md:hidden w-full items-center justify-center">
         {/* @ts-ignore */}
---
+++ src/images/Arrow.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+const SvgIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="21"
+    height="60"
+    fill="none"
+    viewBox="0 0 21 60"
+  >
+    <path
+      fill="#B48149"
+      fillOpacity="0.4"
+      d="M0 30 20.33 0 9.903 30 20.33 60z"
+    ></path>
+  </svg>
+);
+
+export default SvgIcon;
